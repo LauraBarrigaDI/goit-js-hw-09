f1db53b234632e59636647931bfa37c6ea468075
LauraBarrigaDI <113401273+LauraBarrigaDI@users.noreply.github.com>
2023-03-23 14:27:17 -0500

    cambio1js

--- js/02-timer.js
+++ js/02-timer.js
@@ -12,3 +12,30 @@ const options = {
 };
 
 flatpickr("#datetime-picker", options);
+
+const currentDate = new Date();
+const selectedDate = selectedDates[0];
+if (selectedDate < currentDate) {
+  window.alert("Please choose a date in the future");
+} else {
+   button.disabled = false;
+  // start countdown
+}
+
+const button = document.getElementById("button");
+button.disabled = true;
+
+let countdownTimer;
+const button = document.getElementById("button");
+startButton.addEventListener("click", () => {
+  const selectedDate = selectedDates[0];
+  countdownTimer = setInterval(() => {
+    const timeRemaining = selectedDate.getTime() - new Date().getTime();
+    if (timeRemaining <= 0) {
+      clearInterval(countdownTimer);
+      // countdown finished
+    } else {
+      // update UI with time remaining
+    }
+  }, 1000);
+});
