e66a0543a23b9b8f200e776ce1186b974ed00b13
LauraBarrigaDI <laurabarriga31@gmail.com>
2023-03-22 22:25:58 -0500

    task1

--- js/01-color-switcher.js
+++ js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+const body = document.body;
 
+let intervalId = null;
+
+startButton.addEventListener('click', startColorSwitcher);
+stopButton.addEventListener('click', stopColorSwitcher);
+
+function startColorSwitcher() {
+  startButton.disabled = true; // desactivar el botón Start mientras se ejecuta el cambio de tema
+  intervalId = setInterval(() => {
+    const color = getRandomHexColor();
+    body.style.backgroundColor = color;
+  }, 1000);
+}
+
+function stopColorSwitcher() {
+  startButton.disabled = false; // habilitar el botón Start
+  clearInterval(intervalId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+}
