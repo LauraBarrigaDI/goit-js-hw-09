366bcc4aeb7aad6045ba6be6abb451a872be6a5c
LauraBarrigaDI <laurabarriga31@gmail.com>
2023-03-22 22:39:05 -0500

    task 3

--- js/03-promises.js
+++ js/03-promises.js
@@ -1,8 +1,35 @@
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+
+  return promise;
 }
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const delayInput = document.querySelector('input[name="delay"]');
+  const stepInput = document.querySelector('input[name="step"]');
+  const amountInput = document.querySelector('input[name="amount"]');
+
+  const firstDelay = Number(delayInput.value);
+  const delayStep = Number(stepInput.value);
+  const amount = Number(amountInput.value);
+
+  for (let i = 1; i <= amount; i++) {
+    const delay = firstDelay + delayStep * (i - 1);
+    createPromise(i, delay)
+      .then(result => console.log(`Promise ${result.position} resolved after ${result.delay} ms`))
+      .catch(error => console.log(`Promise ${error.position} rejected after ${error.delay} ms`));
+  }
+});
