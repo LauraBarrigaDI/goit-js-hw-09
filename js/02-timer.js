cab30c9b49753dcc72f98be0fa7aeee95d3f6de4
LauraBarrigaDI <113401273+LauraBarrigaDI@users.noreply.github.com>
2023-03-23 14:32:13 -0500

    cambio2js

--- js/02-timer.js
+++ js/02-timer.js
@@ -39,3 +39,27 @@ startButton.addEventListener("click", () => {
     }
   }, 1000);
 });
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+};
+
+function addLeadingZero(value) {
+  return value.toString().padStart(2, "0");
+};
+
