b5f789ff6a3e0fe648efe5ba2bc90a2d0ed33763
LauraBarrigaDI <113401273+LauraBarrigaDI@users.noreply.github.com>
2023-03-23 15:03:34 -0500

    cambio4js

--- js/02-timer.js
+++ js/02-timer.js
@@ -1,10 +1,31 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 
-const flatpickr = require("flatpickr");
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+};
+
+function addLeadingZero(value) {
+  return value.toString().padStart(2, "0");
+};
 
 const button = document.getElementById("start-button");
-   button.addEventListener("click",() => {
+button.addEventListener("click",() => {
   const selectedDate = selectedDates[0];
   countdownTimer = setInterval(() => {
     const timeRemaining = selectedDate.getTime() - new Date().getTime();
@@ -29,30 +50,8 @@ const options = {
       window.alert("Please choose a date in the future");
     } else {
       button.disabled = false;
-      
-  
-
-function convertMs(ms) {
-  // Number of milliseconds per unit of time
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-
-  // Remaining days
-  const days = Math.floor(ms / day);
-  // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-  return { days, hours, minutes, seconds };
-};
-
-function addLeadingZero(value) {
-  return value.toString().padStart(2, "0");
+    }
+  }
 };
 
 flatpickr("#datetime-picker", options);
